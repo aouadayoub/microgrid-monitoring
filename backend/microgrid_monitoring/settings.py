64857de44e059bdd9cb98f7f26c64ee876fec32f
aouad ayoub <aouadayoub85@gmail.com>
2025-08-22 12:12:38 +0100

    initial project steup

--- backend/microgrid_monitoring/settings.py
+++ backend/microgrid_monitoring/settings.py
@@ -40,6 +40,8 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    'rest_framework',
+    'energy',
 ]
 
 MIDDLEWARE = [
@@ -121,6 +123,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = "static/"
 
 # Default primary key field type
